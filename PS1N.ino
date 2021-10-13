3b62d5b54d2b1a9a84520a75f3ff8725ffa9fff3
Micle Sergiu <micle_sergiu7@yahoo.ro>
2021-10-13 18:09:18 +0300

    Am adaugat timer

--- PS1N.ino
+++ PS1N.ino
@@ -3,11 +3,17 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 unsigned int temp;
+int h=12;
+int m=59;
+int s=45;
+
+static uint32_t last_time, now = 0;
 
 void setup()
 {
  lcd.begin(16,2);
  adc_init();
+ now=millis();
 }
 
 void adc_init()
@@ -38,6 +44,57 @@ void loop()
  lcd.print("Temp:");
  lcd.setCursor(5, 0);
  lcd.print(tC);
- delay(1000);
- lcd.clear();
+ lcd.print((char)178);
+ lcd.print("C");
+ lcd.setCursor(0,1);
+ lcd.print("Ora:");
+ lcd.setCursor(4,1);
+ if(h<10)lcd.print("0");
+ lcd.print(h);
+ lcd.print(":");
+ if(m<10)lcd.print("0");
+ lcd.print(m);
+ lcd.print(":");
+ if(s<10)lcd.print("0");
+ lcd.print(s);
+  
+ for ( int i=0 ;i<5 ;i++)
+{
+  	while ((now-last_time)<200)
+  	{ 
+     now=millis();
+  	}
+  
+ 	 last_time=now;
+
+ 	if(s==60)
+ 	{
+  	 s=0;
+  	 m=m+1;
+ 	}
+ 	if(m==60)
+ 	{
+  	 m=0;
+ 	 h=h+1;
+ 	}
+ 	if(h==24)
+ 	{
+  	 h=0;
+ 	}  
+ }
+  s++;
+  if(s==60)
+ 	{
+  	 s=0;
+  	 m=m+1;
+ 	}
+ 	if(m==60)
+ 	{
+  	 m=0;
+ 	 h=h+1;
+ 	}
+ 	if(h==24)
+ 	{
+  	 h=0;
+ 	}
 }
